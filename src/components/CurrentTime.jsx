780aeb4bb7a2e34915793ed3799259801db18d7d
Andi Kolari <AndiKolari627@gmail.com>
2023-03-13 23:51:38 -0400

    Added functionality where refetch timer will persist after page refresh using localStorage

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -5,21 +5,29 @@ function CurrentTime({dataTime, setDataTime, fetchAPI, fetchTime, setFetchTime})
 
     
     
-    
-
-
-
     function clickHandler(e) {
-       
-
-    
-      if(Date.now()/1000-fetchTime>300){
+      if(!!localStorage.getItem('myTime')){
+        if(Date.now()/1000-localStorage.getItem('myTime')>300){
+          fetchAPI();
+          setFetchTime(Date.now()/1000);
+          localStorage.setItem('myTime', Date.now()/1000);
+          
+        }
+        else{
+          alert(`You must wait 5 minutes before refreshing. You have ${((parseFloat(localStorage.getItem('myTime')))+300-Date.now()/1000).toFixed(0)} seconds remaining`)
+          
+        }
+      }else {if(Date.now()/1000-fetchTime>300){
         fetchAPI();
         setFetchTime(Date.now()/1000);
+        localStorage.setItem('myTime', Date.now()/1000);
+      
       }
       else{
         alert(`You must wait 5 minutes before refreshing. You have ${(fetchTime+300-Date.now()/1000).toFixed(0)} seconds remaining`)
+        
       }
+    }
 
       
     }
